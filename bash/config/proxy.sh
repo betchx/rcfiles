1edc4e34bde629d5e4bcd8882926bfc761f955b3
Atsushi Tanabe <tanabe.atsushi@nikken.jp>
2014-11-27 15:43:17 +0900

    ネットワークの条件によりプロクシ設定の有無を修正可能に
    後から設定可能とするために，
    * 判定＋更新部
    * プロクシ設定部
    * プロクシ設定削除部
    を個別の関数とした．

--- bash/config/proxy.sh
+++ bash/config/proxy.sh
@@ -1,9 +1,38 @@
 # Proxy
 
-address=192.168.166.12
-port=8080
+export address=192.168.166.12
+export port=8080
 
-export HTTP_PROXY=$address:$port
-export https_proxy=$address:$port
+setup_proxy()
+{
+  export HTTP_PROXY=$address:$port
+  export https_proxy=$address:$port
+  echo proxy of $HTTP_PROXY will be used.
+}
 
+reset_proxy()
+{
+  unset HTTP_PROXY
+  unset https_proxy
+  echo No Proxy will be used.
+}
+
+function update_proxy()
+{
+  CURRENT_IP=`ipconfig | grep IPv | awk -F ': ' 'NR==1{print $2}'`
+  echo Current IP address is \"$CURRENT_IP\"
+  case $CURRENT_IP in
+    10.1.166.*)
+      setup_proxy
+      ;;
+    192.168.166.*)
+      setup_proxy
+      ;;
+    *)
+      reset_proxy
+      ;;
+  esac
+}
+
+update_proxy
 
