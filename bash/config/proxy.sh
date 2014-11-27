5c0a79dacfcd799537f190edb705247b4571edf5
Atsushi Tanabe <tanabe.atsushi@nikken.jp>
2014-11-27 15:44:39 +0900

    後から機能を呼び出しやすくするために関数の名前を変更
    全てproxy_から始まる様にし，タブ補完で出てきやすい様に配慮．

--- bash/config/proxy.sh
+++ bash/config/proxy.sh
@@ -3,36 +3,36 @@
 export address=192.168.166.12
 export port=8080
 
-setup_proxy()
+proxy_setup()
 {
   export HTTP_PROXY=$address:$port
   export https_proxy=$address:$port
   echo proxy of $HTTP_PROXY will be used.
 }
 
-reset_proxy()
+proxy_reset()
 {
   unset HTTP_PROXY
   unset https_proxy
   echo No Proxy will be used.
 }
 
-function update_proxy()
+proxy_update()
 {
   CURRENT_IP=`ipconfig | grep IPv | awk -F ': ' 'NR==1{print $2}'`
   echo Current IP address is \"$CURRENT_IP\"
   case $CURRENT_IP in
     10.1.166.*)
-      setup_proxy
+      proxy_setup
       ;;
     192.168.166.*)
-      setup_proxy
+      proxy_setup
       ;;
     *)
-      reset_proxy
+      proxy_reset
       ;;
   esac
 }
 
-update_proxy
+proxy_update
 
