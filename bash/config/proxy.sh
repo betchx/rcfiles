c171f0800547167bbc3ae26db8abaaf3bbb39e60
tanabe <tanabe.atsushi@nikken.jp>
2018-09-04 10:35:06 +0900

    プロクシ判定について修正．
    wpad.datを参考に東京と大阪の両方に対応する様にした．
    わかりやすい様にwpad.datの関連部分をコピー．
    大阪はサブネットマスクが23だったので，IPの3つ目が2種類存在することに対応させた．

--- bash/config/proxy.sh
+++ bash/config/proxy.sh
@@ -1,11 +1,8 @@
 # Proxy
 
-export ns_proxy_address=192.168.166.12
-export ns_proxy_port=8080
-
 proxy_setup()
 {
-  export HTTP_PROXY=$ns_proxy_address:$ns_proxy_port
+  export HTTP_PROXY=$1
   export HTTPS_PROXY=$HTTP_PROXY
   git config --global http.proxy http://$HTTP_PROXY
   git config --global https.proxy http://$HTTP_PROXY
@@ -25,16 +22,29 @@ proxy_update()
 {
   CURRENT_IP=`ipconfig | grep -a IPv | awk -F ': ' 'NR==1{print $2}'`
   echo Current IP address is \"$CURRENT_IP\"
+  # "  # 左のクォートはsakuraエディタの色付けバグへの対応
   case $CURRENT_IP in
-    10.1.6.*)
-      export ns_proxy_address=192.168.3.5
-      proxy_setup
+#	// 東京後楽園ビル　シビル
+#          if (isInNet(ipaddr, KourakuNSCLANip, sub24) ||
+#              isInNet(ipaddr, KourakuNSCWirelessip, sub24))
+#                return ProxyTokyoKouraku;
+#var ProxyTokyoKouraku = "PROXY 192.168.1.5:8080; PROXY 192.168.3.5:8080";
+#var KourakuNSCLANip = "192.168.6.0";
+#var KourakuNSCWirelessip = "10.1.6.0";
+#var sub24 = "255.255.255.0";
+    192.168.6.* | 10.1.6.*)
+      proxy_setup 192.168.1.5:8080
       ;;
-    10.1.166.*)
-      proxy_setup
-      ;;
-    192.168.166.*)
-      proxy_setup
+#	// 大阪シビル
+#          if (isInNet(ipaddr, OsakaNSCLANip, sub23) ||
+#              isInNet(ipaddr, OsakaNSCWirelessip, sub23))
+#                return ProxyOsakaNSC;
+#var ProxyOsakaNSC = "PROXY 192.168.166.12:8080; PROXY ocp16:8080";
+#var OsakaNSCLANip = "192.168.166.0";
+#var OsakaNSCWirelessip = "10.1.166.0";
+#var sub23 = "255.255.254.0";
+    192.168.166.* | 192.168.167.* | 10.1.166.* | 10.1.167.*)
+      proxy_setup 192.168.166.12:8080
       ;;
     *)
       proxy_reset
