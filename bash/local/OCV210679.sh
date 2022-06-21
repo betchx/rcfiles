3387ece99d015cdf3d3277a818be479b364d1f57
tanabe@ocv210679 <tanabe.atsushi@gmail.com>
2022-06-21 09:27:42 +0900

    OCV210679向けの修正
    Rubyの設定が主．
    古いRubyを使用する様にしている．

---
+++ bash/local/OCV210679.sh
@@ -0,0 +1,7 @@
+
+TOP=/C/Softs/Ruby4Exerb
+RB=$TOP/ruby-1.8.7-p374-i386-mingw32
+DEVKIT=$TOP/DevKit-tdm-32-4.5.2-20111229-1559-sfx
+
+export PATH=$PATH:$RB/bin:$DEVKIT/\bin:$DEVKIT/mingw/bin:$DEVKIT/mingw/mingw32/bin
+
